99b057add3553d0b9eb70f8b139cf17314eabaee
btopo <bret.topo@gmail.com>
2023-06-11 20:12:55 -0400

    Added Edit method for Posts

--- DotnetAPI/Controllers/PostController.cs
+++ DotnetAPI/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 using DotnetAPI.Data;
+using DotnetAPI.Dtos;
 using DotnetAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -77,6 +78,47 @@ namespace DotnetAPI.Controllers
          return _dapper.LoadData<Post>(sql);  
       }
 
-      
+      [HttpPost("Post")]
+      public IActionResult AddPost(PostToAddDto postToAdd)
+      {
+          string sql = @"
+          INSERT INTO TutorialAppSchema.Posts(
+              [UserId],
+              [PostTitle],
+              [PostContent],
+              [PostCreated],
+              [PostUpdated]) VALUES(" + this.User.FindFirst("userId")?.Value
+              + ",'" + postToAdd.PostTitle
+              + "','" + postToAdd.PostContent
+              + "', GETDATE(), GETDATE() )";
+
+          if (_dapper.ExecuteSql(sql))
+          {
+              return Ok();
+          } 
+
+          throw new Exception("Failed to create new post!");   
+
+      }
+
+      [HttpPut("Post")]
+      public IActionResult EditPost(PostToEdit postToEdit)
+      {
+          string sql = @"
+          UPDATE TutorialAppSchema.Posts 
+              SET PostContent = '"+ postToEdit.PostContent +
+              "', PostTitle = '" + postToEdit.PostTitle +
+               @"', PostUpdated = GETDATE()
+                    WHERE PostId = " + postToEdit.PostId.ToString() +
+                    "AND UserId = " + this.User.FindFirst("userId")?.Value;      
+
+          if (_dapper.ExecuteSql(sql))
+          {
+              return Ok();
+          } 
+
+          throw new Exception("Failed to edit new post!");   
+
+      }
   }
 }
