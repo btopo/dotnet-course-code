b70bde3d0a9a6d18abcd2ca5eaef08ec585fb1c0
btopo <bret.topo@gmail.com>
2023-06-25 19:25:01 -0400

    updated registration endpoint in auth controller

--- DotnetAPI/Dtos/UserForRegistrationDto.cs
+++ DotnetAPI/Dtos/UserForRegistrationDto.cs
@@ -8,6 +8,9 @@ namespace DotnetAPI.Dtos
       public string FirstName { get; set; }
       public string LastName { get; set; }
       public string Gender { get; set; }
+      public string JobTitle { get; set; }
+      public string Department { get; set; }
+      public decimal Salary { get; set; }
       
  
       public UserForRegistrationDto()
@@ -36,6 +39,14 @@ namespace DotnetAPI.Dtos
          if (PasswordConfrim == null)
       {
           PasswordConfrim = "";
+      }
+         if (JobTitle == null)
+      {
+        JobTitle = "";
+      }
+      if (Department == null)
+      {
+        Department = "";
       }
       }
   }
